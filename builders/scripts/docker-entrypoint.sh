ed18c66cf688b06ae6ee717aa92d18691d8ffc7d
Tulio Gonçalves <tuliogoncalves@gmail.com>
2023-09-09 19:07:16 -0400

    refactoring all sail

---
+++ builders/scripts/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# We'll say that we are by default in dev
+sed -i 's/^display_errors\s*=.*/display_errors = On/g' /etc/php/current/fpm/conf.d/30-custom-php.ini
+sed -i 's/^max_execution_time\s*=.*/max_execution_time = -1/g' /etc/php/current/fpm/conf.d/30-custom-php.ini
+
+cd storage
+mkdir -p logs
+mkdir -p framework
+mkdir -p framework/cache
+mkdir -p framework/cache/data
+mkdir -p framework/sessions
+mkdir -p framework/testing
+mkdir -p framework/views
+cd ..
+
+echo "-----------------------"
+echo "-----------------------"
+echo "INSTALAÇÃO FINALIZADA."
+echo "-----------------------"
+echo "-----------------------"
+
+# exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+echo "$@"
+
+exec "$@"
